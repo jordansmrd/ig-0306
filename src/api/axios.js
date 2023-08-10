2f0a4956f68bb3fc254e47d3ae036ddb419825e8
Jordan <jordansumardi@hotmail.com>
2023-08-10 15:31:49 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 export const api = axios.create({
  baseURL: 'https://mock-api-ig2.glitch.me/',
  headers: {
-  authorization: process.env.REACT_APP_FIREBASE_auth
+  authorization: 'MAKANBAKSO'
  }
 });
